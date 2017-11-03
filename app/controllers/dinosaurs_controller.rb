c3492da3f38de44abeee82316bdf0ca9f688eec3
“Tea <teateearu@gmail.com>
2017-11-03 16:29:32 +0100

    Edit, update, delete dinosaurs

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -12,7 +12,6 @@ class DinosaursController < ApplicationController
   end
 
   def create
-    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
 
     @dinosaur = Dinosaur.new(dinosaur_params)
 
@@ -22,4 +21,33 @@ class DinosaursController < ApplicationController
        render 'new'
     end
   end
+
+  def edit
+   @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def update
+   @dinosaur = Dinosaur.find(params[:id])
+
+   if @dinosaur.update_attributes(dinosaur_params)
+     redirect_to @dinosaur
+   else
+     render 'edit'
+   end
+ end
+
+ def destroy
+    @dinosaur = Dinosaur.find(params[:id])
+
+    @dinosaur.destroy
+
+    redirect_to dinosaurs_path
+  end
+
+ private
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :age, :image_url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root "dinosaurs#index"
 
-  get "dinosaurs" => "dinosaurs#index"
-  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
-  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
-
-  post "dinosaurs" => "dinosaurs#create"
+ resources :dinosaurs
+  # get "dinosaurs" => "dinosaurs#index"
+  # get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  # get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  # post "dinosaurs" => "dinosaurs#create"
+  # get 'dinosaurs/:id/edit' => 'dinosaurs#edit', as: :edit_dinosaur
+  # patch "dinosaurs/:id" => "dinosaurs#update"
+  # delete "dinosaurs/:id" => "dinosaurs#destroy"
 
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
