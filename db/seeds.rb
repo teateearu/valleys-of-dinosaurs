966077341adfa353a55a11aef9fdcd73ef2bad6e
“Tea <teateearu@gmail.com>
2017-11-03 15:26:35 +0100

    Create show and new dinosaur pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
+dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
+dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "Dinofour", age: rand(100)})
+dino5 = Dinosaur.create!( { name: "Dinofive", age: rand(100)})
